5cfe65e0491b82cbb940f30826a660119c0e4abd
bryce-torres <justinbrycetorres@gmail.com>
2025-10-16 19:16:46 +0800

    added rough calc in seeding protocol

--- script.js
+++ script.js
@@ -20,3 +20,19 @@ function openCity(evt, cityName) {
 }
 
 document.getElementById("defaultOpen").click();
+
+/*Math Part*/
+const cell_count = document.getElementById('cell_count');
+const cell_density = document.getElementById('cell_density');
+const amount_to_add = document.getElementById('amount_to_add');
+
+function update_density() {
+            const cell_count = document.getElementById('cell_count');
+            val = (cell_count.value/2)*10000
+            cell_density.value = val.toExponential(2);
+            const standard_count = document.getElementById('standard_count');
+            val2 = (standard_count.value/cell_density.value)*1000;
+            amount_to_add.value = val2.toFixed(0);
+
+        }
+setInterval(update_density, 100);
